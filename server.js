e9b7910b583f8042e7e638deaabe19089c67fa1f
Randy Olshen <rolshen@aol.com>
2020-02-20 18:27:31 -0800

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-var PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 const mainDir = path.join(__dirname, "/public");
 
